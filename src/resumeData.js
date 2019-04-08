ca9d5a75ac6c209f7d573c47c5b51aa895bbac41
emitdlr <emit@utexas.edu>
2019-04-08 16:01:31 -0500

    First full passthrough

---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+  // "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Emit De La Rosa",
+  "role": "Frontend Developer",
+  "linkedinId":"https://www.linkedin.com/in/emit-de-la-rosa-b732965/",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/emit-de-la-rosa-b732965/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/emitdlr",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"https://join.skype.com/invite/w4X6XVYW9dLk",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"B.A graduate from the University of Texas at Austin in Liberal Arts. After 15 years in Finance, I have changed careers and now am a Full Stack Developer.",
+  "address":"Austin, Texas",
+  "website":"http://www.emitdelarosa.com",
+  "education":[
+    {
+      "UniversityName":"University of Texas at Austin CPE",
+      "specialization":"Full Stack Deverlopment certification",
+      "MonthOfPassing":"July",
+      "YearOfPassing":"2018",
+      // "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"University of Texas at Austin",
+      "specialization":"B.A. Liberal Arts",
+      "MonthOfPassing":"December",
+      "YearOfPassing":"2010",
+      // "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Sunpower",
+      "specialization":"Compliance and Treasury Analyst",
+      "MonthOfLeaving":"July",
+      "YearOfLeaving":"2017",
+      // "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Dell Financial Services",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  // "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
