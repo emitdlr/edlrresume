9c5827412f4a34081b6f5190ae620f5b20b99f8f
Emit De La Rosa <emit@utexas.edu>
2020-05-16 12:22:26 -0500

    fixing netlify github transfer

--- src/components/contactus/contactus.js
+++ src/components/contactus/contactus.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 export default class ContactUs extends Component {
   render() {
-    let resumeData = this.props.resumeData;
+    // eslint-disable-next-line
+    // let resumeData = this.props.resumeData;
     return (
       <section id="contact">
           <div className="row section-head center">
