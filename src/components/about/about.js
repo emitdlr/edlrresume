5b399174ab2a85630c1c66fa62ab42c1a117d935
Emit De La Rosa <emit@utexas.edu>
2021-02-26 10:39:33 -0600

    run prod build

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -35,7 +35,7 @@ export default class About extends Component {
                     <br></br>
                     <span>{resumeData.website1}</span>
                     <br></br>
-                    <span>{resumeData.website2}</span>
+                    <span>{resumeData.website2 }</span>
        					   </p>
                   </div>
                </div>
